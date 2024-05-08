5d6572b78f5293ca7fcc03ccc762c1f06156d956
Usamakhan2004 <155829515+Usamakhan2004@users.noreply.github.com>
2024-05-08 11:06:40 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+let menuIcon = document.getElementById("menu-icon");
+let menuCut = document.getElementById("menuCut");
+let dropDown = document.getElementById("drop-down");
+let ddi = document.getElementById("ddi");
+let downIcon = document.getElementById("down-icon");
+let skills = document.getElementById("skills");
+let menu = document.getElementById("menu");
+let menuOpen = true;
+let dropOpen = true;
+let down = true;
+
+menuIcon.addEventListener("click", handleClick);
+menuCut.addEventListener("click", handleClick);
+dropDown.addEventListener("click", handleDropDown);
+downIcon.addEventListener("click", handleDownClick);
+
+function handleClick(){
+    if(menuOpen){
+        menuOpen = false;
+        menu.style.left = "0px";
+    }else{
+        menuOpen = true;
+        menu.style.left = "-260px";
+    }
+}
+function handleDropDown(){
+    if(dropOpen){
+        dropOpen = false;
+        ddi.style.height = "130px";
+    }else{
+        dropOpen = true;
+        ddi.style.height = "0px";
+    }
+}
+
+function handleDownClick(e){
+    if(down){
+        e.target.src = "../assets/up.png";
+        skills.className = "skill-box show";
+        down = false;
+    }else{
+        e.target.src = "../assets/down.png";
+        skills.className = "skill-box hide";
+        down = true;
+    }
+}
