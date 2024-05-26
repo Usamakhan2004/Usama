7134fe8b72525cee67717ebcc36c5bb332684d6d
Usamakhan2004 <155829515+Usamakhan2004@users.noreply.github.com>
2024-05-25 23:29:25 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ let menuIcon = document.getElementById("menu-icon");
 let menuCut = document.getElementById("menuCut");
 let dropDown = document.getElementById("drop-down");
 let ddi = document.getElementById("ddi");
-let downIcon = document.getElementById("down-icon");
-let skills = document.getElementById("skills");
 let menu = document.getElementById("menu");
+
 let menuOpen = true;
 let dropOpen = true;
 let down = true;
@@ -12,7 +11,6 @@ let down = true;
 menuIcon.addEventListener("click", handleClick);
 menuCut.addEventListener("click", handleClick);
 dropDown.addEventListener("click", handleDropDown);
-downIcon.addEventListener("click", handleDownClick);
 
 function handleClick(){
     if(menuOpen){
@@ -30,17 +28,5 @@ function handleDropDown(){
     }else{
         dropOpen = true;
         ddi.style.height = "0px";
-    }
-}
-
-function handleDownClick(e){
-    if(down){
-        e.target.src = "assets/up.png";
-        skills.className = "skill-box show";
-        down = false;
-    }else{
-        e.target.src = "assets/down.png";
-        skills.className = "skill-box hide";
-        down = true;
     }
 }
