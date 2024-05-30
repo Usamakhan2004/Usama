03c834f5054dfb428f0b5fb97c3c501e8c0c26f7
Usamakhan2004 <155829515+Usamakhan2004@users.noreply.github.com>
2024-05-29 21:21:50 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ let menuCut = document.getElementById("menuCut");
 let dropDown = document.getElementById("drop-down");
 let ddi = document.getElementById("ddi");
 let menu = document.getElementById("menu");
+let dwnBtn = document.getElementById("dwn-btn");
+let dwnbBox = document.getElementById("dwn-btns-box");
+let dwnCutBtn = document.getElementById("dwn-cut-btn");
 
 let menuOpen = true;
 let dropOpen = true;
 let down = true;
+let dwnShown = true;
 
 menuIcon.addEventListener("click", handleClick);
 menuCut.addEventListener("click", handleClick);
 dropDown.addEventListener("click", handleDropDown);
+dwnBtn.addEventListener("click", handleDownloadButton);
+dwnCutBtn.addEventListener("click", handleDownloadButton);
 
 function handleClick(){
     if(menuOpen){
@@ -24,9 +30,19 @@ function handleClick(){
 function handleDropDown(){
     if(dropOpen){
         dropOpen = false;
-        ddi.style.height = "130px";
+        ddi.style.height = "175px";
     }else{
         dropOpen = true;
         ddi.style.height = "0px";
     }
+}
+function handleDownloadButton(){
+    if(dwnShown){
+        dwnShown = false;
+        dwnbBox.style.visibility = "visible";
+    }else{
+        dwnShown = true;
+        dwnbBox.style.visibility = "hidden";
+    }
+    console.log("Ok");
 }
